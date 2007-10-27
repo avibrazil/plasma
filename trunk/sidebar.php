e2d4d511d93aba56db7c64ded432535f5906d113
aviram <aviram@59be43b5-793d-0410-a0f9-99f7e0cbaa54>
2007-10-27 20:53:45 +0000

    Sidebar starting to happen.

--- trunk/sidebar.php
+++ trunk/sidebar.php
@@ -2,10 +2,70 @@
 
 /*
 Template Name: Sidebar
+
+$Id$
+
 */
 
-// Initialize the Widgets subsystem.
+// Initialize Sidebars.
 register_sidebar(array(
+	'name' => 'header-1'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'header-2'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'footer-1'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'footer-2'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'main-singlepost'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'main-multipost'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'real-1'
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2 class="widgettitle">',
+	'after_title' => '</h2>',
+));
+
+register_sidebar(array(
+	'name' => 'real-2'
 	'before_widget' => '<div id="%1$s" class="widget %2$s">',
 	'after_widget' => '</div>',
 	'before_title' => '<h2 class="widgettitle">',
@@ -14,68 +74,4 @@ register_sidebar(array(
 
 
 
-
-
-if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) {
-
-	// This code won't be executed if you have the Widgets plugin.
-	// But its designed to work correctly even if you don't have the plugin.
-	
-	widget_soleil_search(array(
- 		'before_widget' => '<div id="soleil-search" class="widget widget_soleil_search">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-	widget_soleil_categories(array(
- 		'before_widget' => '<div id="soleil-categories" class="widget widget_soleil_categories">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-	widget_soleil_archives(array(
- 		'before_widget' => '<div id="soleil-archives" class="widget widget_soleil_archives">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-	if(is_home()) {
-		?><div class="widget_about widget" id="about">
-			<h2 class="widgettitle"><?php _e('About this Blog','theme'); ?></h2>
-			<?php _e(get_bloginfo('description'),'personal'); ?>
-		</div><?php
-	}
-
-	widget_soleil_links(array(
-		'before_widget' => '<div id="soleil-links" class="widget widget_soleil_links">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-	widget_soleil_subscriptions(array(
- 		'before_widget' => '<div id="soleil-subscriptions" class="widget widget_soleil_subscriptions">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-	widget_soleil_meta(array(
- 		'before_widget' => '<div id="soleil-meta" class="widget widget_soleil_meta">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-        ));
-
-
-	if (function_exists('wp_theme_switcher')) {
-		?><div class="widget" id="theme-switcher">
-			<h2 class="widgettitle"><?php _e('Themes:','theme'); ?></h2>
-			<?php wp_theme_switcher(); ?>
-		</div><?php
-	}
-
-} ?>
+?>
