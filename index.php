70d915bbf19e7c1e9ae0c655ed386a943f4228a0
aviram <aviram@59be43b5-793d-0410-a0f9-99f7e0cbaa54>
2007-11-01 16:07:22 +0000

    More changes but nothing's working yet.

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ $context=Context::getContext();
 
 get_header();
 
+//print_r($context);
 
-$context->panels['header-1']->render();
-$context->panels['header-2']->render();
+//$context->panels['header-1']->render();
+//$context->panels['header-2']->render();
 
-$context->panels['main-multipost']->render();
+//$context->panels['main-multipost']->render();
+print_r($context);
 
 $context->panels['footer-1']->render();
 $context->panels['footer-2']->render();
