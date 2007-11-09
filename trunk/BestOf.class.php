b7b016d2fa0171b854f9e180cc35e8266cadc895
aviram <aviram@59be43b5-793d-0410-a0f9-99f7e0cbaa54>
2007-11-09 20:24:32 +0000

    Starting to get rid of classes... too much overhead.

--- trunk/BestOf.class.php
+++ trunk/BestOf.class.php
@@ -1,22 +1,25 @@
 <?php
 /*
-
 $Id$
 */
 
-function BestOf_render($args,$ident) {
-	extract($args);
-	$tabs=BestOf::$created[$ident]->tabs;
 
+$BestPosts_cssClassName="widgetBestPosts";
+$BestPosts_wpOptions="widget_bestposts";
+
+
+
+function BestPosts_render($args,$instance) {
+	extract($args);
+	//$tabs=BestOf::$created[$instance]->tabs;
+
+	$tabs = get_option($BestPosts_wpOptions);
 
 	echo($before_widget . "\n");
 
-	print_r($args);
-	print_r("num=$ident");
-	print_r("tabs=$tabs");
-
+	$index=0;
 	echo("<ul class=\"tabs\">\n");
-	foreach ($tabs as $tab) {
+	foreach ($tabs[$instance] as $tab) {
 		echo("<li id=\"tab-" . $index . "\">" . $tab['title'] . "</li>\n");
 		$index++;
 	}
@@ -25,17 +28,18 @@ function BestOf_render($args,$ident) {
 
 	$index=0;
 	echo("<div class=\"pop-in\">\n");
-	foreach ($tabs as $tab) {
-		query_posts("cat=" . $tab['tag'] . "&order=ASC");
+	foreach ($tabs[$instance] as $tab) {
+		query_posts("tag=" . $tab['tag'] . "&order=DESC&showposts=15");
 		echo("<ul id=\"list-$index\">\n");
 		if (have_posts()) :
 			while (have_posts()) : the_post();
 				echo("<li>");
 				the_title();
-				echo("</li>");
+				echo("</li>\n");
 			endwhile;
 		endif;
 		echo("</ul>\n");
+		$index++;
 	}
 	echo("</div>\n");
 
@@ -47,8 +51,9 @@ function BestOf_render($args,$ident) {
 
 
 
-function BestOf_control($id) {
-	$options = $newoptions = get_option('widget_bestof');
+function BestPosts_control($params) {
+	extract($params);
+	$options = $newoptions = get_option($BestPosts_wpOptions);
 
 	if ( !is_array($options) )
 		$options = $newoptions = array();
@@ -59,7 +64,7 @@ function BestOf_control($id) {
 
 	if ( $options != $newoptions ) {
 		$options = $newoptions;
-		update_option('widget_bestof', $options);
+		update_option($BestPosts_wpOptions, $options);
 	}?>
 
 <script type="text/javascript">
@@ -78,12 +83,14 @@ function removeTabSelected(id) {
 }
 
 function appendTab(id) {
-	var tabTitle=document.getElementById('new-tab-title-' + id).value;
-	var tagName=document.getElementById('new-tab-tag-' + id).value;
+	var tabTitle=document.getElementById("new-tab-title-" + id).value;
+	var tagName =document.getElementById("new-tab-tag-"   + id).value;
 	
   var elOptNew = document.createElement('option');
   elOptNew.text = tabTitle + " (tag: " + tagName + ")";
-  elOptNew.value = tabTitle + "|" + tagName;
+  elOptNew.value = tagName + "|" + tabTitle;
+	elOptNew.id = id + "-" + tagName;
+	elOptNew.onclick = "optionSelected('" + id + "','" + elOptNew.id + "');";
   var elSel = document.getElementById("tabs-" + id);
 
   try {
@@ -93,32 +100,53 @@ function appendTab(id) {
   }
 }
 
+
+function optionSelected(id) {
+	var elemSelect=document.getElementById("tabs-" + id);
+
+  for (var i = elemSelect.length - 1; i>=0; i--) {
+    if (elemSelect.options[i].selected) {
+			var tabtag=elemSelect.options[i].value.split("|");
+			document.getElementById("new-tab-tag-" + id).value=tabtag[0];
+			document.getElementById("new-tab-title-" + id).value=tabtag[1];
+			document.getElementById("current-tab-tag-" + id).value=elemSelect.options[i].id;
+
+			break;
+    }
+  }
+}
+
 //]]>
 </script>
-<table border="1"><tr><td style="width: 80%">
-<select style="width: 90%" multiple="multiple" name="tabs-<?php echo("$id"); ?>" id="tabs-<?php echo("$id"); ?>">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="fiat">Fiat</option>
-  <option value="audi">Audi</option>
-</select>
+<table border="1"><tr><td style="width: 50%">
+<select name="tabs-<?php echo("$id"); ?>" id="tabs-<?php echo("$id"); ?>"
+	onchange="optionSelected('<?php echo("$id"); ?>')"
+	style="width: 100%" size="5"></select>
 </td>
+
 <td>
-<input type="button" value="&uarr;" onclick=""/><br/>
-<input type="button" value="&darr;" onclick=""/>
+<input type="button" value="&uArr;" onclick=""/><br/>
+<input type="button" value="&dArr;" onclick=""/>
 </td>
-</tr><tr>
+</tr>
+
+<tr>
 <td>
-<input type="button" value="Add tab" onclick="appendTab('tabs-<?php echo("$id"); ?>')"/> 
-<input type="button" value="Delete tab" onclick="removeOptionSelected('tabs-<?php echo("$id"); ?>')"/>
+<input type="button" value="Add tab" onclick="appendTab('<?php echo("$id"); ?>')"/> 
+<input type="button" value="Delete tab" onclick="removeTabSelected('<?php echo("$id"); ?>')"/>
 </td>
-</tr><tr>
+</tr>
+
+<tr>
 <td>
+<input type="hidden" id="current-tab-tag-<?php echo("$id"); ?>" value=""/>
+<input type="hidden" id="<?php echo("$id"); ?>-tabs-tags" value=""/> <!-- the one field that matters -->
 <label for="new-tab-title-<?php echo("$id"); ?>">New tab title</label><br/>
 <input style="width: 80%;" name="new-tab-title-<?php echo("$id"); ?>" id="new-tab-title-<?php echo("$id"); ?>" type="text"/><br/>
 <label for="new-tab-tag-<?php echo("$id"); ?>">New tab title</label><br/>
 <input style="width: 80%;" name="new-tab-tag-<?php echo("$id"); ?>" id="new-tab-tag-<?php echo("$id"); ?>" type="text"/>
 </td>
+
 <td>
 <input type="button" value="Update selected" onclick=""/>
 </td>
@@ -131,6 +159,32 @@ function appendTab(id) {
 
 	<?php
 }
+
+
+
+
+
+function appendTab($instance,$title,$tag) {
+	$tabs=get_option($BestPosts_wpOptions);
+	
+	$index=sizeof($tabs[$instance]);
+	$tabs[$instance][$index]=array('title' => $title, 'tag' => $tag);
+
+	update_option($BestPosts_wpOptions, $tabs);
+}
+
+
+
+
+function BestPosts_register($instance,$name) {
+	$options['classname']=$BestPosts_cssClassName;
+	$options['params']=$instance;
+	wp_register_sidebar_widget($instance,$name,'BestPosts_render',$options);
+
+	$dims = array('width' => 500, 'height' => 350);
+	wp_register_widget_control($instance,$name,'BestPosts_control',$dims);
+}
+
 
 
 class BestOf extends Widget {
@@ -154,17 +208,40 @@ class BestOf extends Widget {
 	public $tabs=array();
 
 	public function __construct($name,$id=0,$register=true) {
-		$params[$id]=$id;
+		$params['id']=$id;
 		BestOf::$created[$id]=$this;
 
-		$dims = array('width' => 600, 'height' => 450);
+		$dims = array('width' => 500, 'height' => 350);
 
 		parent::__construct($name,$id,'BestOf_render','widget_bestof',$params,'BestOf_control',$dims,$params);
+
+		$options = get_option('widget_bestof');
 	}
+
+
+
+
+
+
 
 	public static function render($args,$id) {
 		BestOf_render($args,$id);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
