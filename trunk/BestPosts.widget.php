c06deec4a308aa5f1ce425cd95d501612714ab44
aviram <aviram@59be43b5-793d-0410-a0f9-99f7e0cbaa54>
2007-11-10 19:44:05 +0000

    daily updates

--- trunk/BestPosts.widget.php
+++ trunk/BestPosts.widget.php
@@ -10,15 +10,21 @@ $BestPosts_wpOptions = "widget_bestposts";
 
 
 function BestPosts_render($args,$instance) {
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+
 	extract($args);
 	//$tabs=BestOf::$created[$instance]->tabs;
 
 	$tabs = get_option($BestPosts_wpOptions);
 
+	if (empty($tabs)) return;
+
 	echo($before_widget . "\n");
 
 	$index=0;
 	echo("<ul class=\"tabs\">\n");
+	echo("instance=$instance\n");
+	print_r($tabs);
 	foreach ($tabs[$instance] as $tab) {
 		echo("<li id=\"tab-" . $index . "\">" . $tab['title'] . "</li>\n");
 		$index++;
@@ -28,6 +34,7 @@ function BestPosts_render($args,$instance) {
 
 	$index=0;
 	echo("<div class=\"pop-in\">\n");
+	echo("instance=$instance\n");
 	foreach ($tabs[$instance] as $tab) {
 		query_posts("tag=" . $tab['tag'] . "&order=DESC&showposts=15");
 		echo("<ul id=\"list-$index\">\n");
@@ -48,11 +55,46 @@ function BestPosts_render($args,$instance) {
 
 
 
+function BestPosts_serialize($instance) {
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+
+	$string="";
+
+	$tabs = get_option($BestPosts_wpOptions);
+
+	if (empty($tabs[$instance])) return $string;
+
+
+	/* Build $string="tag1~Title 1^tag2~Title 2^tag3~Title 3" */
+	foreach($tabs[$instance] as $fullTab) {
+		if (!empty($string)) $string.="^";
+		$string.=$fullTab['tag'] . "~" . $fullTab['title'];
+	}
+	return $string;
+}
+
+
+function BestPosts_unserialize($instance,$string) {
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+	
+	$final[$instance]=array();
+	$unified=split("^",$string);
+	
+	foreach ($unified as $pair) {
+		$temp=split("~",$pair);
+		$final[$instance]['tag']=$temp[0];
+		$final[$instance]['title']=$temp[1];
+	}
+
+	return $final;
+}
 
 
 
-function BestPosts_control($params) {
-	extract($params);
+function BestPosts_control($id) {
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+//	extract($params);
+
 	$options = $newoptions = get_option($BestPosts_wpOptions);
 
 	if ( !is_array($options) )
@@ -71,6 +113,54 @@ function BestPosts_control($params) {
 //<![CDATA[
 
 // Reference: http://www.mredkj.com/tutorials/tutorial005.html
+
+
+// This array is filled by tabsUnserialize() function and 
+var BestPosts_tabs=new Array();
+BestPosts_tabs['<?php echo("$id"); ?>']=new Array();
+
+
+
+/** Unserialize the hidden <input id="${id}-tabs-tags"/> */
+function tabsUnserialize(id) {
+	var serElem=document.getElementById(id + "-tabs-tags");
+	var array1;
+
+	array1=serElem.value.split("^");
+	for (var i=0; i<array1.length; i++) {
+		var array2=array1[i].split("~");
+
+		BestPosts_tabs[id][i]['tag']=array2[0];
+		BestPosts_tabs[id][i]['title']=array2[1];
+	}
+}
+
+
+/** Rebuilds the UI based on BestPosts_tabs[id] array. */
+function tabsRebuildUI(id) {
+	var elSel = document.getElementById(id + "-tabs");
+	var elOpt;
+
+	// First remove all.
+	while (elOpt=elSel.childNodes[0]) elSel.removeChild(elOpt);
+
+	// Now create new options based on the BestPosts_tabs[id] array
+	for (var i=0; i<BestPosts_tabs[id].length; i++) {
+	  elOpt = document.createElement('option');
+
+		elOpt.text = BestPosts_tabs[id][i]['title'] + " (tag: " + BestPosts_tabs[id][i]['tag'] + ")";
+		elOpt.value = i;
+
+		try {
+			// standards compliant; doesn't work in IE
+			elSel.add(elOpt, null);
+		} catch(ex) {
+			// IE only
+			elSel.add(elOpt);
+		}
+	}
+}
+
 
 function removeTabSelected(id) {
   var elSel = document.getElementById("tabs-" + id);
@@ -91,13 +181,14 @@ function appendTab(id) {
   elOptNew.value = tagName + "|" + tabTitle;
 	elOptNew.id = id + "-" + tagName;
 	elOptNew.onclick = "optionSelected('" + id + "','" + elOptNew.id + "');";
-  var elSel = document.getElementById("tabs-" + id);
 
-  try {
-    elSel.add(elOptNew, null); // standards compliant; doesn't work in IE
-  } catch(ex) {
-    elSel.add(elOptNew); // IE only
-  }
+	var elSel = document.getElementById(id + "-tabs");
+
+	try {
+		elSel.add(elOptNew, null); // standards compliant; doesn't work in IE
+	} catch(ex) {
+		elSel.add(elOptNew); // IE only
+	}
 }
 
 
@@ -119,7 +210,7 @@ function optionSelected(id) {
 //]]>
 </script>
 <table border="1"><tr><td style="width: 50%">
-<select name="tabs-<?php echo("$id"); ?>" id="tabs-<?php echo("$id"); ?>"
+<select id="<?php echo("$id"); ?>-tabs"
 	onchange="optionSelected('<?php echo("$id"); ?>')"
 	style="width: 100%" size="5"></select>
 </td>
@@ -139,21 +230,28 @@ function optionSelected(id) {
 
 <tr>
 <td>
-<input type="hidden" id="current-tab-tag-<?php echo("$id"); ?>" value=""/>
-<input type="hidden" id="<?php echo("$id"); ?>-tabs-tags" value=""/> <!-- the one field that matters -->
-<label for="new-tab-title-<?php echo("$id"); ?>">New tab title</label><br/>
-<input style="width: 80%;" name="new-tab-title-<?php echo("$id"); ?>" id="new-tab-title-<?php echo("$id"); ?>" type="text"/><br/>
-<label for="new-tab-tag-<?php echo("$id"); ?>">New tab title</label><br/>
-<input style="width: 80%;" name="new-tab-tag-<?php echo("$id"); ?>" id="new-tab-tag-<?php echo("$id"); ?>" type="text"/>
+<!-- The one field that matters. All the rest is just UI. -->
+<input type="hidden" id="<?php echo("$id"); ?>-tabs-tags" value="<?php echo(BestPosts_serialize($id)); ?>"/>
+
+<input type="hidden" id="<?php echo("$id"); ?>-current-selection" value=""/>
+
+<label for="<?php echo("$id"); ?>-title-edit">Tab title</label><br/>
+<input style="width: 80%;" name="<?php echo("$id"); ?>-title-edit" id="<?php echo("$id"); ?>-title-edit" type="text"/><br/>
+<label for="<?php echo("$id"); ?>-tag-edit">Tag containing posts</label><br/>
+<input style="width: 80%;" name="<?php echo("$id"); ?>-tag-edit" id="<?php echo("$id"); ?>-tag-edit" type="text"/>
 </td>
 
 <td>
-<input type="button" value="Update selected" onclick=""/>
+<input type="button" value="Update selected" onclick="tabsRebuildUI('<?php echo("$id"); ?>');"/>
 </td>
 </tr>
 </table>
 
-
+<script type="text/javascript">
+//<![CDATA[
+tabsRebuildUI('<?php echo("$id"); ?>');
+//]]>
+</script>
 
 
 
@@ -165,6 +263,8 @@ function optionSelected(id) {
 
 
 function BestPosts_appendTab($instance,$title,$tag) {
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+
 	$tabs=get_option($BestPosts_wpOptions);
 	
 	$index=sizeof($tabs[$instance]);
@@ -177,14 +277,14 @@ function BestPosts_appendTab($instance,$title,$tag) {
 
 
 function BestPosts_register($instance,$name) {
-	print_r($BestPosts_cssClassName);
+	global $BestPosts_cssClassName, $BestPosts_wpOptions;
+
 	$opt['classname']=$BestPosts_cssClassName;
 	$opt['params']=$instance;
-	print_r($opt);
 	wp_register_sidebar_widget($instance,$name,'BestPosts_render',$opt);
 
 	$dims = array('width' => 500, 'height' => 350);
-	wp_register_widget_control($instance,$name,'BestPosts_control',$dims);
+	wp_register_widget_control($instance,$name,'BestPosts_control',$dims,$instance);
 }
 
 
